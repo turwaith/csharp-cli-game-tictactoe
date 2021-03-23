67b127b329c019eb4acf65fb4da0400f936d7c79
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-23 17:25:17 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_game_tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Csharp game Tic Tac Toe
+
+A command line interface Tic Tac Toe game developed for learning purposes
+Written in c# (.NET Core 5.0)
