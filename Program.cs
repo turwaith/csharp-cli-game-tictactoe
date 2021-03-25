bb2e1679539069b2ac40f359edf539ab080f2636
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-25 14:29:28 +0100

    fix bug when user enter played case

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace csharp_game_tictactoe
             Random rand = new Random();
             int userCase, iaCase;
 
+            Console.Clear();
             Console.WriteLine("***** Jeu du Morpion *****");
             Console.WriteLine("───────\n│0│1│2│\n───────\n│3│4│5│\n───────\n│6│7│8│\n───────");
             Console.Write("Entrez une case (0 à 8)\nTapez une touche pour commencer");
@@ -21,13 +22,11 @@ namespace csharp_game_tictactoe
             while(true)
             {            
                 // user
-                userCase = GetCase("Entrez la case voulue: ");
+                userCase = GetCase("Entrez la case voulue: ", UnPlayedCases);
+                boardGame[userCase] = CaseStates.Circle;
 
-                if(IsEmpty(boardGame, userCase, CaseStates.Circle))
-                {
-                    DisplayBoard(boardGame);
-                    UnPlayedCases.Remove(userCase);
-                }                    
+                DisplayBoard(boardGame);
+                UnPlayedCases.Remove(userCase);                   
 
                 if(IsWin(boardGame, CaseStates.Circle))
                 {
@@ -42,13 +41,11 @@ namespace csharp_game_tictactoe
                 }                    
 
                 // ia
-                iaCase = rand.Next(0,UnPlayedCases.Count);                
+                iaCase = rand.Next(0,UnPlayedCases.Count);
+                boardGame[UnPlayedCases[iaCase]] = CaseStates.Cross;
 
-                if(IsEmpty(boardGame, UnPlayedCases[iaCase], CaseStates.Cross))                                
-                {
-                    DisplayBoard(boardGame);
-                    UnPlayedCases.Remove(UnPlayedCases[iaCase]);                       
-                }                
+                DisplayBoard(boardGame);
+                UnPlayedCases.Remove(UnPlayedCases[iaCase]);           
 
                 if(IsWin(boardGame, CaseStates.Cross))
                 {
@@ -63,30 +60,34 @@ namespace csharp_game_tictactoe
             Empty, Circle, Cross
         }
 
-        static int GetCase(string msg, int min = 0, int max = 8)
+        /// <summary>
+        /// Ask for a case and check if it's not alreay played
+        /// Convert String to Int and check if is in List(int)
+        /// </summary>
+        /// <param name="msg">The message to display when asking</param>
+        /// <param name="unPlayedCase">List of cases not played</param>
+        /// <returns>Return a verified case</returns>
+        static int GetCase(string msg, List<int> unPlayedCase)
         {
             int output = 0;
 
-            // loop while input is not integer and between 0 and 8
+            // loop while input is not integer
             do
             {
                 Console.Write(msg);
-            } while (!int.TryParse(Console.ReadLine(),out output) || (output < min) || (output > max));
+            } while (!int.TryParse(Console.ReadLine(),out output));
+            
+            // check if case is not already played
+            if(!unPlayedCase.Contains(output))
+                return GetCase(msg, unPlayedCase);
 
             return output;
         }
 
-        static bool IsEmpty(CaseStates[] boardGame, int gridCase, CaseStates player)
-        {
-            if(boardGame[gridCase] == CaseStates.Empty)
-            {
-                boardGame[gridCase] = player;
-                return true;
-            }
-            
-            return false;
-        }
-
+        /// <summary>
+        /// Display the board game with a for loop
+        /// </summary>
+        /// <param name="boardGame">CaseStates array</param>
         static void DisplayBoard(CaseStates[] boardGame)
         {
             Console.Clear();
@@ -112,21 +113,24 @@ namespace csharp_game_tictactoe
             }            
             Console.WriteLine("│\n───────");
         }
+
+        /// <summary>
+        /// Check if a player has won
+        /// </summary>
+        /// <param name="boardGame">CaseStates array</param>
+        /// <param name="player">CaseStates player</param>
+        /// <returns></returns>
         static bool IsWin(CaseStates[] boardGame, CaseStates player)
         {           
             // test i
-            for(int i = 0, size = boardGame.Length; i < size; i+= 3)
-            {
+            for(int i = 0, size = boardGame.Length; i < size; i+= 3)            
                 if(boardGame[i] == player && boardGame[i+1] == player && boardGame[i+2] == player)
-                    return true;
-            }
+                    return true;            
 
             // test collumns
-            for(int i = 0; i < 3; i++)
-            {
+            for(int i = 0; i < 3; i++)            
                 if(boardGame[i] == player && boardGame[i+3] == player && boardGame[i+6] == player)
-                    return true;
-            }
+                    return true;            
 
             // test oblique left
             if(boardGame[0] == player && boardGame[4] == player && boardGame[8] == player)
